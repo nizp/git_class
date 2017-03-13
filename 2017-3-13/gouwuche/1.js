a07f39ca4779736ec07fda35d01e28a7ed1f25dd
nizp <nzp890524@163.com>
2017-03-13 18:06:06 +0800

    3-13

---
+++ 2017-3-13/gouwuche/1.js
@@ -0,0 +1,54 @@
+const ul1 = document.getElementById('ul1'),
+ul2 = document.getElementById('ul2');
+let li = ul1.children;
+var arr = [];
+let list = Array.from(li);
+//console.log(arr)
+list.forEach(function(e,i){
+	e.onclick = function(){
+		//找到li的2个span（一个是价格，一个是名字）
+		var span = e.getElementsByTagName('span');
+		
+		//查找数组中有没有当前点击的商品名
+		var onOff = arr.some((a)=>{
+			return a.name == span[0].innerHTML
+		});
+		
+		//如果当前点击的商品，已经在数组中了，那么就不要push。
+		if(!onOff){
+			arr.push({
+				name:span[0].innerHTML,
+				price:span[1].innerHTML
+			});
+			//当把数组添加完后，还需要把数据放到本地存储中
+			localStorage.setItem('sp',JSON.stringify(arr));
+			//渲染
+			render();
+		}
+	};
+});
+render();
+
+	//关联数据，在数据发生变化的时候，重新渲染页面
+	
+	window.addEventListener('storage',()=>{
+		render();
+	});
+	
+
+function render(){
+	//看看本地存储有没有数据，如果没有，arr还是走[]，如果有走数据
+	if(JSON.parse(localStorage.getItem('sp'))){
+		arr = JSON.parse(localStorage.getItem('sp'));
+	}
+	ul2.innerHTML = '';
+	var html = '';
+	//循环渲染。
+	arr.forEach((e,i)=>{
+		html += `<li>
+					<span>${e.name}</span>
+					<span>${e.price}</span>
+				</li>`;
+	});
+	ul2.innerHTML = html;
+}
